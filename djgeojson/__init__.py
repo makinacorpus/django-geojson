fb91b7b339b981d5ef301e90b21a56c73dbc413d
Mathieu Leplatre <mathieu.leplatre@makina-corpus.com>
2013-07-09 15:03:14 +0200

    Add __version__ for package

--- djgeojson/__init__.py
+++ djgeojson/__init__.py
@@ -1,1 +1,8 @@
+#: Module version, as defined in PEP-0396.
+pkg_resources = __import__('pkg_resources')
+distribution = pkg_resources.get_distribution('django-geojson')
+
+__version__ = distribution.version
+
+
 GEOJSON_DEFAULT_SRID = 4326
