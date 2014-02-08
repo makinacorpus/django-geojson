5af1af00a06d22230f0d2d2c244b3d5e9ced4a65
Mathieu Leplatre <contact@mathieu-leplatre.info>
2014-02-08 12:16:31 +0100

    Back to development: 2.3.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ here = os.path.abspath(os.path.dirname(__file__))
 
 setup(
     name='django-geojson',
-    version='2.3.0',
+    version='2.3.1.dev0',
     author='Mathieu Leplatre',
     author_email='mathieu.leplatre@makina-corpus.com',
     url='https://github.com/makinacorpus/django-geojson',
