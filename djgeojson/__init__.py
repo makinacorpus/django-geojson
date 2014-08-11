51c6ee7fd9a59cc9e47237a3630f7478eaa935af
nippo <n@ippo.io>
2014-08-11 18:27:21 +0200

    Adds a warning for "Module version, as defined in PEP-0396"

--- djgeojson/__init__.py
+++ djgeojson/__init__.py
@@ -1,8 +1,11 @@
 #: Module version, as defined in PEP-0396.
 pkg_resources = __import__('pkg_resources')
-distribution = pkg_resources.get_distribution('django-geojson')
-
-__version__ = distribution.version
-
+try:
+    distribution = pkg_resources.get_distribution('django-geojson')
+    __version__ = distribution.version
+except:
+    __version__ = 'unknown'
+    import warnings
+    warnings.warn('No distribution found.')
 
 GEOJSON_DEFAULT_SRID = 4326
