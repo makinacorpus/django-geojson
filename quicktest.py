4127d2be0216b9121118988e29a0600d40d48403
Mathieu Leplatre <mathieu@mozilla.com>
2015-04-15 11:28:43 +0200

    Test with more versions in Travis

--- quicktest.py
+++ quicktest.py
@@ -44,8 +44,17 @@ class QuickDjangoTest(object):
             },
             INSTALLED_APPS=self.INSTALLED_APPS + self.apps,
         )
-        from django.test.simple import DjangoTestSuiteRunner
-        failures = DjangoTestSuiteRunner().run_tests(self.apps, verbosity=1)
+
+        if django.VERSION >= (1, 7, 0):
+            # see: https://docs.djangoproject.com/en/dev/releases/1.7/#standalone-scripts
+            django.setup()
+        if django.VERSION >= (1, 6, 0):
+            # see: https://docs.djangoproject.com/en/dev/releases/1.6/#discovery-of-tests-in-any-test-module
+            from django.test.runner import DiscoverRunner as Runner
+        else:
+            from django.test.simple import DjangoTestSuiteRunner as Runner
+
+        failures = Runner().run_tests(self.apps, verbosity=1)
         if failures:  # pragma: no cover
             sys.exit(failures)
 
