2fc3c3f103e51343d8205b1e4709d05d5d01fea3
Mathieu Leplatre <mathieu@mozilla.com>
2015-04-17 13:55:38 +0200

    Preparing release 2.8.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ here = os.path.abspath(os.path.dirname(__file__))
 
 setup(
     name='django-geojson',
-    version='2.7.1.dev0',
+    version='2.8.0',
     author='Mathieu Leplatre',
     author_email='mathieu.leplatre@makina-corpus.com',
     url='https://github.com/makinacorpus/django-geojson',
