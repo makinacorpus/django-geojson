9c57d6723a32482c9e39686edaf1737933c780e4
Tocho Tochev <tocho@tochev.net>
2016-08-21 16:31:05 +0300

    Python 3.5 support
    Closes https://github.com/makinacorpus/django-geojson/issues/80

--- setup.py
+++ setup.py
@@ -32,5 +32,7 @@ setup(
                  'Framework :: Django',
                  'Development Status :: 5 - Production/Stable',
                  'Programming Language :: Python :: 2.7',
-                 'Programming Language :: Python :: 3.3'],
+                 'Programming Language :: Python :: 3.3',
+                 'Programming Language :: Python :: 3.4',
+                 'Programming Language :: Python :: 3.5'],
 )
