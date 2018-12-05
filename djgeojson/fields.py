4d3e84951d0c043121f9c4e8fa1e88d9127ec11d
Gagaro <gagaro42@gmail.com>
2018-12-05 14:17:18 +0100

    fix flake8 + update travis conf

--- djgeojson/fields.py
+++ djgeojson/fields.py
@@ -96,10 +96,3 @@ class PolygonField(GeometryField):
 
 class MultiPolygonField(GeoJSONField):
     geom_type = 'MULTIPOLYGON'
-
-
-try:
-    from south.modelsinspector import add_introspection_rules
-    add_introspection_rules([], ["^djgeojson\.fields\.(GeoJSONField|GeometryField|GeometryCollectionField|PointField|MultiPointField|LineStringField|MultiLineStringField|PolygonField|MultiPolygonField)"])
-except ImportError:
-    pass
