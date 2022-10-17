133a7773b2bcdfe3c4ef7397a0048cb7eae68574
Gagaro <gagaro42@gmail.com>
2022-10-17 11:06:00 +0200

    4.0.0

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ here = os.path.abspath(os.path.dirname(__file__))
 
 setup(
     name='django-geojson',
-    version='3.2.2.dev',
+    version='4.0.0',
     author='Mathieu Leplatre',
     author_email='mathieu.leplatre@makina-corpus.com',
     url='https://github.com/makinacorpus/django-geojson',
